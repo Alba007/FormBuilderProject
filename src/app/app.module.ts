56a1d08fd3f4a1c8f026780e75ec4db508171789
Samanta Beqiri <samanta29beqiri@gmail.com>
2019-09-23 14:57:22 +0200

    after merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PropertiesComponent } from './properties/properties.component';
+import {AppComponent} from './app.component';
+import {PropertiesComponent} from './properties/properties.component';
 
-import { DynamicFormsMaterialUIModule } from '@ng-dynamic-forms/ui-material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormComponent } from './form/form.component';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { FormControlComponent } from './form-control/form-control.component';
-import { AllFormsModule } from './all-forms/all-forms.module';
+import {DynamicFormsMaterialUIModule} from '@ng-dynamic-forms/ui-material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormComponent} from './form/form.component';
+import {MatListModule} from '@angular/material/list';
+import {FormControlComponent} from './form-control/form-control.component';
+import {AllFormsModule} from './all-forms/all-forms.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertiesComponent,
     FormComponent,
-    FormControlComponent
+    FormControlComponent,
     FormComponent,
-
 
 
   ],
@@ -38,4 +36,5 @@ import { AllFormsModule } from './all-forms/all-forms.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
