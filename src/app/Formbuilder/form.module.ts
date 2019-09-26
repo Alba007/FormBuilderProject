c1ba74d5e9ec9e67bef53beabc8f482b84667488
Nikolin Koco <nikolin.koco@outlook.com>
2019-09-26 18:35:37 +0200

    css

--- src/app/Formbuilder/form.module.ts
+++ src/app/Formbuilder/form.module.ts
@@ -8,6 +8,7 @@ import { FormComponent } from './form/form.component';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { FormControlComponent } from './form-control/form-control.component';
+import {MatCardModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { FormControlComponent } from './form-control/form-control.component';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   exports: [
